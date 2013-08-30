082c82b90f76bdff20b9f0f790ce800df45c5a6b
Martin Len <mlen1990@Martins-MacBook-Air-2.local>
2013-08-30 14:01:17 -0700

    Added an algorithm to reverse a string

---
+++ ReverseString.java
@@ -0,0 +1,25 @@
+/* ReverseString.java */
+
+import java.io.StringWriter;
+
+public class ReverseString {
+
+  /**
+   *  To call this method in another method use
+   *  "ReverseString.reverseString(str)"
+   *
+   *  @param str the string to be reversed.
+   *
+   *  @return the reversed version of the input string.
+   **/
+  public static String reverseString(String str) {
+    if (str == null) {
+      return null;
+    }
+    StringWriter writer = new StringWriter();
+    for (int i = str.length() - 1; i > -1; i--) {
+      writer.append(str.charAt(i));
+    }
+    return writer.toString();
+  }
+}
